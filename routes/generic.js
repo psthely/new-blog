2e43cff963b1c37813c10023d59a4329698e77ff
psthely <psthely.webdev@gmail.com>
2020-06-26 22:43:07 +0200

    add markdown-it module; create posts directory for .md; get post content from posts/[post].md in generic.js router

--- routes/generic.js
+++ routes/generic.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var md = require('markdown-it')();
 
 var post = {
-  date: 'April 25, 2017',
-  title: 'This is a generic page',
-  description: 'Aenean ornare velit lacus varius enim ullamcorper proin aliquam facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor amet nullam sed etiam veroeros.',
-  image: {
-    src: 'images/pic01.jpg',
-    alt: ''
+    date: 'April 25, 2017',
+    title: 'This is a generic page',
+    description: 'Aenean ornare velit lacus varius enim ullamcorper proin aliquam facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor amet nullam sed etiam veroeros.',
+    image: {
+      src: 'images/pic01.jpg',
+      alt: ''
   },
-  content: '<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sapien risus, commodo eget turpis at, elementum convallis enim turpis, lorem ipsum dolor sit amet nullam.</p>\
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus. Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum.</p>'
+    content: ''
 }
+
+// get the post content from /public/posts
+fs.readFile(path.join(__dirname, '../public/posts', 'generic.md'),
+  'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    post.content = md.render(data);
+});
 
 router.get('/', function(req, res) {
   res.render(
